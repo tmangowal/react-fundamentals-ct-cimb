9c63cf23e762a69b16b24e7067d527181ef493db
tmangowal <tmangowal@gmail.com>
2020-04-15 10:44:52 +0700

    :disappointed: Moved functions outside of render

--- src/views/screens/AuthScreen.jsx
+++ src/views/screens/AuthScreen.jsx
@@ -13,7 +13,101 @@ class AuthScreen extends React.Component {
     activeEditIdx: null,
   };
 
-  asd = () => {};
+  inputHandler = (e, field) => {
+    this.setState({ [field]: e.target.value });
+  };
+
+  registerHandler = () => {
+    const { repPassword, password, username, users } = this.state;
+    if (repPassword == password) {
+      let newData = {
+        username,
+        password,
+      };
+
+      this.setState({
+        users: [...users, newData],
+        username: "",
+        password: "",
+        repPassword: "",
+      });
+
+      console.log(users);
+    } else {
+      alert("Password belum cocok");
+    }
+  };
+
+  loginHandler = () => {
+    const { loginUsername, loginPassword, users } = this.state;
+    for (let i = 0; i < users.length; i++) {
+      if (
+        users[i].username == loginUsername &&
+        users[i].password == loginPassword
+      ) {
+        this.setState({
+          isLoggedIn: true,
+          currentUsername: users[i].username,
+          loginUsername: "",
+          loginPassword: "",
+        });
+        break;
+      }
+
+      if (i == users.length - 1) {
+        alert("User tidak ada atau password salah");
+      }
+    }
+  };
+
+  deleteHandler = (idx) => {
+    const { users } = this.state;
+    let temp = [...users];
+
+    temp.splice(idx, 1);
+
+    this.setState({ users: temp });
+  };
+
+  renderUsers = () => {
+    const { users, activeEditIdx } = this.state;
+    return users.map((val, idx) => {
+      if (idx == activeEditIdx) {
+        return (
+          <tr>
+            <td>{idx + 1}</td>
+            {/* <td>{val.username}</td> */}
+            <td>
+              <input type="text" placeholder={val.username} />
+            </td>
+            <td>
+              <input
+                type="button"
+                value="Delete"
+                className="btn btn-danger"
+                onClick={() => this.deleteHandler(idx)}
+              />
+            </td>
+          </tr>
+        );
+      } else {
+        return (
+          <tr>
+            <td>{idx + 1}</td>
+            <td>{val.username}</td>
+            <td>
+              <input
+                type="button"
+                value="Edit"
+                className="btn btn-info"
+                onClick={() => this.setState({ activeEditIdx: idx })}
+              />
+            </td>
+          </tr>
+        );
+      }
+    });
+  };
 
   render() {
     const {
@@ -28,98 +122,6 @@ class AuthScreen extends React.Component {
       loginUsername,
     } = this.state;
 
-    const inputHandler = (e, field) => {
-      this.setState({ [field]: e.target.value });
-    };
-
-    const registerHandler = () => {
-      if (repPassword == password) {
-        let newData = {
-          username,
-          password,
-        };
-
-        this.setState({
-          users: [...users, newData],
-          username: "",
-          password: "",
-          repPassword: "",
-        });
-
-        console.log(users);
-      } else {
-        alert("Password belum cocok");
-      }
-    };
-
-    const loginHandler = () => {
-      for (let i = 0; i < users.length; i++) {
-        if (
-          users[i].username == loginUsername &&
-          users[i].password == loginPassword
-        ) {
-          this.setState({
-            isLoggedIn: true,
-            currentUsername: users[i].username,
-            loginUsername: "",
-            loginPassword: "",
-          });
-          break;
-        }
-
-        if (i == users.length - 1) {
-          alert("User tidak ada atau password salah");
-        }
-      }
-    };
-
-    const deleteHandler = (idx) => {
-      let temp = [...users];
-
-      temp.splice(idx, 1);
-
-      this.setState({ users: temp });
-    };
-
-    const renderUsers = () => {
-      return users.map((val, idx) => {
-        if (idx == activeEditIdx) {
-          return (
-            <tr>
-              <td>{idx + 1}</td>
-              {/* <td>{val.username}</td> */}
-              <td>
-                <input type="text" placeholder={val.username} />
-              </td>
-              <td>
-                <input
-                  type="button"
-                  value="Delete"
-                  className="btn btn-danger"
-                  onClick={() => deleteHandler(idx)}
-                />
-              </td>
-            </tr>
-          );
-        } else {
-          return (
-            <tr>
-              <td>{idx + 1}</td>
-              <td>{val.username}</td>
-              <td>
-                <input
-                  type="button"
-                  value="Edit"
-                  className="btn btn-info"
-                  onClick={() => this.setState({ activeEditIdx: idx })}
-                />
-              </td>
-            </tr>
-          );
-        }
-      });
-    };
-
     return (
       <div>
         <h1>Auth Screen</h1>
@@ -131,27 +133,27 @@ class AuthScreen extends React.Component {
               className="form-control mt-2"
               type="text"
               placeholder="Username"
-              onChange={(e) => inputHandler(e, "username")}
+              onChange={(e) => this.inputHandler(e, "username")}
             />
             <input
               value={password}
               className="form-control mt-2"
               type="text"
               placeholder="Password"
-              onChange={(e) => inputHandler(e, "password")}
+              onChange={(e) => this.inputHandler(e, "password")}
             />
             <input
               value={repPassword}
               className="form-control mt-2"
               type="text"
               placeholder="Repeat Password"
-              onChange={(e) => inputHandler(e, "repPassword")}
+              onChange={(e) => this.inputHandler(e, "repPassword")}
             />
             <input
               type="button"
               value="Register"
               className="btn btn-primary mt-3"
-              onClick={registerHandler}
+              onClick={this.registerHandler}
             />
           </div>
           <table className="table">
@@ -162,7 +164,7 @@ class AuthScreen extends React.Component {
                 <th>Action</th>
               </tr>
             </thead>
-            <tbody>{renderUsers()}</tbody>
+            <tbody>{this.renderUsers()}</tbody>
           </table>
           {/* <div className="card p-5" style={{ width: "400px" }}>
             <h4>Login</h4>
@@ -171,20 +173,20 @@ class AuthScreen extends React.Component {
               className="form-control mt-2"
               type="text"
               placeholder="Username"
-              onChange={(e) => inputHandler(e, "loginUsername")}
+              onChange={(e) => this.inputHandler(e, "loginUsername")}
             />
             <input
               value={loginPassword}
               className="form-control mt-2"
               type="text"
               placeholder="Password"
-              onChange={(e) => inputHandler(e, "loginPassword")}
+              onChange={(e) => this.inputHandler(e, "loginPassword")}
             />
             <input
               type="button"
               value="Login"
               className="btn btn-primary mt-3"
-              onClick={loginHandler}
+              onClick={this.loginHandler}
             />
           </div>
           {isLoggedIn ? <h2>Welcome {currentUsername}</h2> : null} */}

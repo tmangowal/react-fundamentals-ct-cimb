3548d704a5b1f886a8f7d73600562d3f165ee563
tmangowal <tmangowal@gmail.com>
2020-04-16 12:06:57 +0700

    :arrow_right: Added redirect on login

--- src/views/screens/AuthScreen.jsx
+++ src/views/screens/AuthScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
 class AuthScreen extends React.Component {
   state = {
@@ -120,77 +120,84 @@ class AuthScreen extends React.Component {
       loginUsername,
     } = this.state;
 
-    return (
-      <div>
-        <h1>Auth Screen</h1>
-        <center className="container">
-          <div className="card p-5" style={{ width: "400px" }}>
-            <h4>Register</h4>
-            <input
-              value={username}
-              className="form-control mt-2"
-              type="text"
-              placeholder="Username"
-              onChange={(e) => this.inputHandler(e, "username")}
-            />
-            <input
-              value={password}
-              className="form-control mt-2"
-              type="text"
-              placeholder="Password"
-              onChange={(e) => this.inputHandler(e, "password")}
-            />
-            <input
-              value={repPassword}
-              className="form-control mt-2"
-              type="text"
-              placeholder="Repeat Password"
-              onChange={(e) => this.inputHandler(e, "repPassword")}
-            />
-            <input
-              type="button"
-              value="Register"
-              className="btn btn-primary mt-3"
-              onClick={this.registerHandler}
-            />
-          </div>
-          {/* <table className="table">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Username</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>{this.renderUsers()}</tbody>
-          </table> */}
-          <div className="card p-5" style={{ width: "400px" }}>
-            <h4>Login</h4>
-            <input
-              value={loginUsername}
-              className="form-control mt-2"
-              type="text"
-              placeholder="Username"
-              onChange={(e) => this.inputHandler(e, "loginUsername")}
-            />
-            <input
-              value={loginPassword}
-              className="form-control mt-2"
-              type="text"
-              placeholder="Password"
-              onChange={(e) => this.inputHandler(e, "loginPassword")}
-            />
-            <input
-              type="button"
-              value="Login"
-              className="btn btn-primary mt-3"
-              onClick={this.loginHandler}
-            />
-          </div>
-          {isLoggedIn ? <h2>Welcome {currentUsername}</h2> : null}
-        </center>
-      </div>
-    );
+    if (!isLoggedIn) {
+      return (
+        <div>
+          <h1>Auth Screen</h1>
+          <center className="container">
+            <div className="card p-5" style={{ width: "400px" }}>
+              <h4>Register</h4>
+              <input
+                value={username}
+                className="form-control mt-2"
+                type="text"
+                placeholder="Username"
+                onChange={(e) => this.inputHandler(e, "username")}
+              />
+              <input
+                value={password}
+                className="form-control mt-2"
+                type="text"
+                placeholder="Password"
+                onChange={(e) => this.inputHandler(e, "password")}
+              />
+              <input
+                value={repPassword}
+                className="form-control mt-2"
+                type="text"
+                placeholder="Repeat Password"
+                onChange={(e) => this.inputHandler(e, "repPassword")}
+              />
+              <input
+                type="button"
+                value="Register"
+                className="btn btn-primary mt-3"
+                onClick={this.registerHandler}
+              />
+            </div>
+            {/* <table className="table">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Username</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>{this.renderUsers()}</tbody>
+            </table> */}
+            <div className="card p-5" style={{ width: "400px" }}>
+              <h4>Login</h4>
+              <input
+                value={loginUsername}
+                className="form-control mt-2"
+                type="text"
+                placeholder="Username"
+                onChange={(e) => this.inputHandler(e, "loginUsername")}
+              />
+              <input
+                value={loginPassword}
+                className="form-control mt-2"
+                type="text"
+                placeholder="Password"
+                onChange={(e) => this.inputHandler(e, "loginPassword")}
+              />
+              <input
+                type="button"
+                value="Login"
+                className="btn btn-primary mt-3"
+                onClick={this.loginHandler}
+              />
+            </div>
+            {isLoggedIn ? <h2>Welcome {currentUsername}</h2> : null}
+          </center>
+        </div>
+      );
+    } else {
+      // return <Redirect to={`/profile/${currentUsername}`} />;
+      return (
+        <Link to={`/profile/${currentUsername}`}>Continue to profile</Link>
+      );
+    }
   }
 }
 
